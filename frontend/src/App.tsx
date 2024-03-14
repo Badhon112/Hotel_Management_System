a1d350bababffd53c4b30eaadb626ec9dfaa6084
badhon112 <biswasbadhon896@gmail.com>
2024-03-14 20:46:37 +0600

    Adding layout

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,38 @@
+import {
+  Navigate,
+  Route,
+  BrowserRouter as Router,
+  Routes,
+} from "react-router-dom";
+import Layout from "./layout/Layout";
+
 const App = () => {
-  return <div className="min-h-screen bg-gray-200">App</div>;
+  return (
+    <Router>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <Layout>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Error,
+                dolore.
+              </p>
+            </Layout>
+          }
+        />
+        <Route
+          path="/search"
+          element={
+            <Layout>
+              <p>Search page</p>
+            </Layout>
+          }
+        />
+        <Route path="*" element={<Navigate to={"/"} />} />
+      </Routes>
+    </Router>
+  );
 };
 
 export default App;
---
+++ frontend/src/components/Hero.tsx
@@ -0,0 +1,14 @@
+const Hero = () => {
+  return (
+    <div className="bg-blue-800 pb-16">
+      <div className="container mx-auto flex flex-col gap-2">
+        <h1 className="text-5xl text-white font-bold">Find your next Room</h1>
+        <p className="text-2xl text-white">
+          Search low prices on hotels for your Vacations
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
