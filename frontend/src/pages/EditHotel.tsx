76478e6fea36ffe4b46cb353796e61b1a1249827
badhon112 <biswasbadhon896@gmail.com>
2024-03-20 11:53:24 +0600

    Edit hotel fixing

--- frontend/src/pages/EditHotel.tsx
+++ frontend/src/pages/EditHotel.tsx
@@ -2,8 +2,10 @@ import { useMutation, useQuery } from "react-query";
 import { useParams } from "react-router-dom";
 import * as apiClient from "../api-client";
 import ManageHotelForm from "../forms/ManageHotelForm/ManageHotelForm";
+import { useAppContext } from "../contexts/AppContext";
 
 const EditHotel = () => {
+  const { showToast } = useAppContext();
   const { hotelId } = useParams();
   const { data: hotel } = useQuery(
     "fetchMyHotelById",
@@ -13,8 +15,12 @@ const EditHotel = () => {
     }
   );
   const { mutate, isLoading } = useMutation(apiClient.updateMyHotelById, {
-    onSuccess: () => {},
-    onError: () => {},
+    onSuccess: () => {
+      showToast({ message: "Hotel Saved!", type: "SUCCESS" });
+    },
+    onError: () => {
+      showToast({ message: "Error Saving Hotel", type: "ERROR" });
+    },
   });
 
   const handleSave = (hotelFormData: FormData) => {
